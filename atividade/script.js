b74909ebf5dd6781788c8dbd0dfe9cb9ae3e619d
ArturH16 <holandaartur163@gmail.com>
2025-12-06 14:44:12 -0300

    Atividade 8,9 e 10

--- atividade/script.js
+++ atividade/script.js
@@ -1,6 +1,7 @@
 let boasVindas = document.getElementById('boas-vindas')
 let hora = new Date().getHours()
 let imagem = document.getElementsByTagName('img')[0]
+//Código para ajustar imagem de acordo com horário
 if ( hora < 6 || hora > 18) {
     imagem.src = 'https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/nationalgeographic2351999-2-min.webp?w=1600&h=1067'
     boasVindas.innerHTML = 'Boa noite'
@@ -15,3 +16,18 @@ else {
     imagem.src = 'https://www.shutterstock.com/image-photo/afternoon-sky-glows-rich-golden-260nw-2623806837.jpg'
     boasVindas.innerHTML = 'Boa tarde'
 }
+
+//Código para redirecionar usuário via formulário
+
+function validarDados(event) {
+    event.preventDefault()
+let usuario = document.getElementById('user').value
+let senha = document.getElementById('pwd').value
+if (usuario === 'admin' && senha === '1234') {
+        location.href = 'sucesso.html'
+    } else {
+        location.href = 'erro.html'
+    }
+}
+
+
